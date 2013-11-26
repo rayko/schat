f5df7e449c626ad13e09284c2d1ac555088d6453
Rayko <rayko.drg@gmail.com>
2013-11-26 16:02:58 -0300

    Implemented nick,text method of handling messages

--- server.rb
+++ server.rb
@@ -4,16 +4,19 @@ EM.run {
   @channel = EM::Channel.new
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
 
-    ws.onopen {
+    ws.onopen { |handshake|
       sid = @channel.subscribe { |msg| ws.send msg }
-      @channel.push "#{sid} connected!"
+      @channel.push "Joined the room."
+      puts "#{sid} connected."
 
       ws.onmessage { |msg|
-        @channel.push "<#{sid}>: #{msg}"
+        @channel.push "[#{msg.split(',').first}] #{msg.split(',').last}"
+        puts "#{sid}: #{msg}"
       }
 
       ws.onclose {
         @channel.unsubscribe(sid)
+        puts "#{sid} left."
       }
     }
 
